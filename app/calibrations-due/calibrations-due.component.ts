de01abb4579055d9e2593dcc70ed0838b87cbc04
Jon Preece <jon@developerhandbook.com>
2016-03-02 16:04:47 +0000

    Added CanActivate decorator

--- app/calibrations-due/calibrations-due.component.ts
+++ app/calibrations-due/calibrations-due.component.ts
@@ -1,9 +1,12 @@
-import { Component } from 'angular2/core';
+import { Component } from "angular2/core";
+import { CanActivate } from "angular2/router";
 
 @Component({
-    templateUrl: 'app/calibrations-due/calibrations-due.component.html',
-    styleUrls: ['app/calibrations-due/styles.css'],
+    templateUrl: "app/calibrations-due/calibrations-due.component.html",
+    styleUrls: ["app/calibrations-due/styles.css"],
 })
+@CanActivate(() => {return true;})
 export class CalibrationsDueComponent {
 
 }
+  
