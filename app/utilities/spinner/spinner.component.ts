d1dee96f30bc814ddee4274818e0c32b44271de3
Jon Preece <jon@developerhandbook.com>
2016-04-01 11:14:58 +0100

    Switched to fading circle

--- app/utilities/spinner/spinner.component.ts
+++ app/utilities/spinner/spinner.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnDestroy} from 'angular2/core';
 
 @Component({
     selector: 'spinner',
-    template: '<div [hidden]="!isDelayedRunning" class="spinner"></div>',
+    template: '<div [hidden]="!isDelayedRunning" class="sk-fading-circle"><div class="sk-circle1 sk-circle"></div><div class="sk-circle2 sk-circle"></div><div class="sk-circle3 sk-circle"></div><div class="sk-circle4 sk-circle"></div><div class="sk-circle5 sk-circle"></div><div class="sk-circle6 sk-circle"></div><div class="sk-circle7 sk-circle"></div><div class="sk-circle8 sk-circle"></div><div class="sk-circle9 sk-circle"></div><div class="sk-circle10 sk-circle"></div><div class="sk-circle11 sk-circle"></div><div class="sk-circle12 sk-circle"></div></div>',
     styleUrls: ["./app/utilities/spinner/spinner.css"]
 })
 export class SpinnerComponent implements OnDestroy {  
