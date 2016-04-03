53aa8efdd0c8897483a6b10f4ed13edb68115153
Jon Preece <jonpreece@hotmail.co.uk>
2016-04-03 17:28:26 +0100

    Fix spinner

--- app/utilities/spinner/spinner.component.ts
+++ app/utilities/spinner/spinner.component.ts
@@ -1,40 +1,15 @@
-import {Component, Input, OnDestroy} from "angular2/core";
+import {Component, Input} from "angular2/core";
 
 @Component({
     selector: "spinner",
     templateUrl: "./app/utilities/spinner/spinner.component.html",
     styleUrls: ["./app/utilities/spinner/spinner.css"]
 })
-export class SpinnerComponent implements OnDestroy {  
-    private currentTimeout: number;
+export class SpinnerComponent {  
     private isDelayedRunning: boolean = false;
 
     @Input()
     public set isRunning(value: boolean) {
-        if (!value) {
-            this.cancelTimeout();
-            this.isDelayedRunning = false;
-            return;
-        }
-
-        if (this.currentTimeout) {
-            return;
-        }
-
-        this.currentTimeout = setTimeout(
-            () => {
-                this.isDelayedRunning = value;
-                this.cancelTimeout();
-            }, 
-            300);
-    }
-
-    public ngOnDestroy(): any {
-        this.cancelTimeout();
-    }
-
-    private cancelTimeout(): void {
-        clearTimeout(this.currentTimeout);
-        this.currentTimeout = undefined;
+        this.isDelayedRunning = value;
     }
 }
