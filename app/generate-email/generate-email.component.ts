ed087725c8392ccacda545c8c83b3232304a0859
Jon Preece <jonpreece@hotmail.co.uk>
2016-04-05 19:35:28 +0100

    Minor tweaking

--- app/generate-email/generate-email.component.ts
+++ app/generate-email/generate-email.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "angular2/core";
-import { CanActivate } from "angular2/router";
-import { hasValidToken } from "../utilities/Jwt";
+import {Component} from "angular2/core";
+import {CanActivate} from "angular2/router";
+import {hasValidToken} from "../utilities/Jwt";
 
 @Component({
     templateUrl: "app/generate-email/generate-email.component.html",
-    styleUrls: ["app/generate-email/styles.css"],
+    styleUrls: ["app/generate-email/styles.css"]
 })
 @CanActivate(() => hasValidToken())
 export class GenerateEmailComponent {
