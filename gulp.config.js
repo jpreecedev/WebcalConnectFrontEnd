6802f8cd337c24a57720667ecd8d900cb9c349b2
Jon Preece <jon@developerhandbook.com>
2016-04-06 14:42:54 +0100

    Added gulp dist task

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,37 @@
-module.exports = function(){
+module.exports = function() {
     var config = {
         temp: '.tmp/',
         tsSource: ["./app/**/*.ts", "!./app/utilities/cookies.ts"],
+        jsSource: ["./app/**/*.js", "!./app/utilities/cookies.js"],
+        rootFiles: [
+            "favicon.ico",
+            "index.html"
+        ],
         scss: [
             './css/site.scss',
             './app/**/*.scss'
-        ]
+        ],
+        libJs: [
+            "node_modules/es6-shim/es6-shim.min.js",
+            "node_modules/systemjs/dist/system-polyfills.js",
+            "node_modules/angular2/es6/dev/src/testing/shims_for_IE.js",
+            "node_modules/angular2/bundles/angular2-polyfills.js",
+            "node_modules/systemjs/dist/system.src.js",
+            "node_modules/rxjs/bundles/Rx.js",
+            "node_modules/angular2/bundles/angular2.dev.js",
+            "node_modules/angular2/bundles/router.dev.js",
+            "node_modules/angular2/bundles/http.dev.js",
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/bootstrap/dist/js/bootstrap.min.js",
+            "node_modules/ng2-pagination/dist/ng2-pagination-bundle.js",
+            "node_modules/bootbox/bootbox.min.js",
+        ],
+        libCss: [
+            "node_modules/bootstrap/dist/css/bootstrap.min.css",
+            "node_modules/font-awesome/css/font-awesome.css"
+        ],
+        dist: "./wwwroot",
+        app: "./app"
     };
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,78 @@
 "use strict";
 
 var gulp = require("gulp");
-var del = require("del");
-var $ = require('gulp-load-plugins')();
 var config = require('./gulp.config.js')();
+var $ = require('gulp-load-plugins')();
 
-gulp.task("tslint", function () {
-    return gulp.src(config.tsSource)
-        .pipe($.tslint())
-        .pipe($.tslint.report("verbose"));
+gulp.task("clean", function() {
+    return gulp.src(config.dist)
+        .pipe($.clean());
 });
 
-gulp.task("clean-css", function (done) {
-    return del(config.temp + "**/*.css");
+gulp.task("lib-css", ["clean"], function() {
+    return gulp.src(config.libCss, { base: "./" })
+        .pipe(gulp.dest(config.dist))
 });
 
-gulp.task("css", ["clean-css"], function () {
-    return gulp.src(config.scss, {base: "./"})
+gulp.task("lib-js", ["lib-css"], function() {
+    return gulp.src(config.libJs, { base: "./" })
+        .pipe(gulp.dest(config.dist))
+})
+
+gulp.task("app-css", ["lib-js"], function() {
+    return gulp.src(config.scss, { base: "./" })
         .pipe($.plumber())
         .pipe($.sass())
         .pipe($.autoprefixer({ browsers: ["last 2 versions", "> 5%"] }))
         .pipe($.plumber.stop())
-        .pipe(gulp.dest("."));
+        .pipe(gulp.dest(config.dist));
 });
 
-gulp.task("scss-watcher", function () {
-    gulp.watch([config.scss], ["css"])
+gulp.task("app-root", ["app-css"], function() {
+    return gulp.src(config.rootFiles, { base: "./" })
+        .pipe(gulp.dest(config.dist))
 });
 
-gulp.task("default", ["css", "tslint"]);
-gulp.task("watch", ["scss-watcher"]);
+gulp.task("app", ["app-root"], function() {
+    return gulp.src(config.jsSource, { base: "./" })
+        .pipe(gulp.dest(config.dist))
+});
+
+gulp.task("dist", ["app"], function() {
+
+});
+
+gulp.task("default", ["dist"]);
+
+// "use strict";
+
+// var gulp = require("gulp");
+// var del = require("del");
+// var $ = require('gulp-load-plugins')();
+// var config = require('./gulp.config.js')();
+
+// gulp.task("tslint", function () {
+//     return gulp.src(config.tsSource)
+//         .pipe($.tslint())
+//         .pipe($.tslint.report("verbose"));
+// });
+
+// gulp.task("clean-css", function (done) {
+//     return del(config.temp + "**/*.css");
+// });
+
+// gulp.task("css", ["clean-css"], function () {
+//     return gulp.src(config.scss, {base: "./"})
+//         .pipe($.plumber())
+//         .pipe($.sass())
+//         .pipe($.autoprefixer({ browsers: ["last 2 versions", "> 5%"] }))
+//         .pipe($.plumber.stop())
+//         .pipe(gulp.dest("."));
+// });
+
+// gulp.task("scss-watcher", function () {
+//     gulp.watch([config.scss], ["css"])
+// });
+
+// gulp.task("default", ["css", "tslint"]);
+// gulp.task("watch", ["scss-watcher"]);
