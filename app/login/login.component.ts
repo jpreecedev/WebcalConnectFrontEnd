e69ca88feeec531247b187c6964cdba13d3b25a1
Jon Preece <jon@developerhandbook.com>
2016-04-07 10:02:41 +0100

    Added animated button to sign in

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -3,14 +3,18 @@ import {Router} from "angular2/router";
 import {HttpService} from "../utilities/HttpService";
 import {hasValidToken} from "../utilities/Jwt";
 import {JwtHelper} from "../utilities/JwtHelper";
+import {AnimatedButtonComponent} from "../utilities/animated-button/animated-button.component";
 
 @Component({
     selector: "login",
     templateUrl: "app/login/login.component.html",
     styleUrls: ["app/login/styles.css"],
-    providers: [HttpService, JwtHelper]
+    providers: [HttpService, JwtHelper],
+    directives: [AnimatedButtonComponent]
 })
 export class LoginComponent implements OnInit {
+
+    private _isRequesting: boolean = false;
 
     constructor(private _httpService: HttpService, private _router: Router, private _jwtHelper: JwtHelper) {
 
@@ -18,11 +22,13 @@ export class LoginComponent implements OnInit {
 
     authenticate(username: string, password: string, rememberMe: boolean): void {
 
+        this._isRequesting = true;
         this._httpService.authenticate(username, password, rememberMe)
             .subscribe((response: boolean) => {
                 if (response) {
                     this._router.navigate(["Dashboard"]);
                 }
+                this._isRequesting = false;
             });
 
     }
