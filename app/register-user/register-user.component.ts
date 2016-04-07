252ae51a17170535e99badfdb51097d69d33014b
Jon Preece <jonpreece@hotmail.co.uk>
2016-04-07 02:48:39 +0100

    Added validation to register form

--- app/register-user/register-user.component.ts
+++ app/register-user/register-user.component.ts
@@ -4,25 +4,47 @@ import {hasValidToken} from "../utilities/Jwt";
 import {HttpService} from "../utilities/HttpService";
 import {Response} from "angular2/http";
 import {RegisterUserService} from "./register-user.service";
+import {AnimatedButtonComponent} from "../utilities/animated-button/animated-button.component";
+
+export interface UserRegistration {
+    emailAddress: string;
+    companyName: string;
+    expiration: string;
+    licenseKey: string;
+}
 
 @Component({
     templateUrl: "app/register-user/register-user.component.html",
     styleUrls: ["app/register-user/styles.css"],
-    providers: [RegisterUserService, HttpService]
+    providers: [RegisterUserService, HttpService],
+    directives: [AnimatedButtonComponent]
 })
-@CanActivate(() => hasValidToken())
+@CanActivate(() => hasValidToken(["Administrator"]))
 export class RegisterUserComponent {
 
-    private _licenseKey: string = "";
+    private _userRegistration: UserRegistration;
+    private _isRequesting: boolean = false;
 
     constructor(private _router: Router, private _service: RegisterUserService) {
-
+        this._userRegistration = {
+            emailAddress: "",
+            companyName: "",
+            expiration: "",
+            licenseKey: ""
+        };
     }
 
     getLicenseKey(expiration: string): void {
         this._service.getLicenseKey(expiration).subscribe((response: Response) => {
-            this._licenseKey = response.json().key;
+            this._userRegistration.licenseKey = response.json().key;
         });
+    }
+
+    submit(): void {
+        this._isRequesting = true;
+        setTimeout(()=>{
+            this._isRequesting = false;
+        }, 3000);
     }
 
     cancel(): void {
