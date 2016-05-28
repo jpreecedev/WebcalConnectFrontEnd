077e2f6e79fc776bda6ec78eed75e837a1193efb
Jon Preece <jonpreece@hotmail.co.uk>
2016-05-28 06:40:52 +0100

    Implement Pikaday

--- app/utilities/date-picker/date-picker.component.ts
+++ app/utilities/date-picker/date-picker.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter, ElementRef} from "@angular/core";
+import {Component, Input, Output, EventEmitter, ElementRef, OnInit} from "@angular/core";
 declare var Pikaday: any;
 
 @Component({
@@ -6,14 +6,27 @@ declare var Pikaday: any;
     templateUrl: "./app/utilities/date-picker/date-picker.component.html",
     styleUrls: ["./app/utilities/date-picker/date-picker.css"]
 })
-export class DatePickerComponent {
-        
-    @Input() date: Date;    
-    @Input() label: string;
-    
-    @Output() dateChanged: EventEmitter<Date> = new EventEmitter();
-    
-    fireChange(){
-        this.dateChanged.emit(this.date);
+export class DatePickerComponent implements OnInit {
+
+    private nativeElement: any;
+
+    constructor(elementRef: ElementRef) {
+        this.nativeElement = elementRef.nativeElement;
     }
+
+    ngOnInit() {
+        var that = this;
+        var picker = new Pikaday({
+            field: this.nativeElement.getElementsByTagName("input")[0],
+            onSelect: function (dt: Date) {
+                that.dateChanged.emit(dt);
+            }
+        });
+    }
+
+    @Input() date: Date;
+
+    @Input() label: string;
+
+    @Output() dateChanged: EventEmitter<Date> = new EventEmitter();
 }
